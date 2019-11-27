3eb7843dd3a4319eca3667fdaa3a01e345bf075e
Kylan <aviananalyst@gmail.com>
2019-11-27 14:30:26 -0800

    Added marriage support, display needs work
    Co-authored-by: Rowan Jacobs <rojacobs@pivotal.io>

--- main.py
+++ main.py
@@ -1,24 +1,42 @@
 import json
 from graphviz import Digraph
-from itertools import chain
 
 
-with open('predicates.json', 'r') as infile:
-    data = json.load(infile)
-
-parsed_data = Digraph(comment="Family tree")
-
-for hash_name in data['hashes']:
-    birth = [year for person, year in data['relations']['born'] if person == hash_name][0]
-    death = [year for person, year in data['relations']['died'] if person == hash_name][0]
-    label = f'{hash_name} \n {birth} - {death}'
-    parsed_data.node(hash_name, label)
-
-for parent, child in data['relations']['parents']:
-    parsed_data.edge(parent, child)
-
-# for partner1, partner2, year in data['relations']['marriages']:
-#     parsed_data.
+def label_maker(hash_name: str, data: dict):
+    try:
+        birth = [year for person, year in data['relations']['born'] if person == hash_name][0]
+    except IndexError:
+        birth = ''
+    try:
+        death = [year for person, year in data['relations']['died'] if person == hash_name][0]
+    except IndexError:
+        death = ''
+    if birth or death:
+        label = f'{hash_name} \n {birth} - {death}'
+    else:
+        label = f'{hash_name}'
+    return label
 
 
-parsed_data.render('output/output.gv', view=True)
+def main():
+    with open('predicates.json', 'r') as infile:
+        data = json.load(infile)
+
+    parsed_data = Digraph(comment="Family tree")
+
+    for hash_name in data['hashes']:
+        label = label_maker(hash_name, data)
+        parsed_data.node(hash_name, label)
+
+    for parent, child in data['relations']['parents']:
+        parsed_data.edge(parent, child)
+
+    for partner1, partner2, year in data['relations']['marriages']:
+        parsed_data.edge(partner1, partner2, label=f'{year}', constraint='false', arrowhead='none')
+
+
+    parsed_data.render('output/output.gv', view=True)
+
+
+if __name__ == '__main__':
+    main()
